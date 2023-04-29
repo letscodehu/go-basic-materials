fb177a6a3026d5beb8c736fadcc190c7887874f7
Krisztian Papp <fejlesztes@letscode.hu>
2023-04-29 14:05:44 +0200

    go-basics-7

--- main.go
+++ main.go
@@ -6,11 +6,27 @@ import (
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("No target directory specified!")
-        fmt.Println("Usage: classifier <directory>")
-        return
-    }
-    directory := os.Args[1]
-    fmt.Println(directory)
+	if len(os.Args) < 2 {
+		fmt.Println("No target directory specified!")
+		fmt.Println("Usage: classifier <directory>")
+		os.Exit(1)
+	}
+	directory := os.Args[1]
+	info, err := os.Stat(directory)
+	if err != nil {
+		fmt.Println(directory, "does not exists.")
+		os.Exit(1)
+	}
+	if !info.IsDir() {
+		fmt.Println(directory, "is not a directory.")
+		os.Exit(1)
+	}
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		fmt.Println(err, "is not a directory.")
+		os.Exit(1)
+	}
+	for index, file := range files {
+		fmt.Println(index, file.Name())
+	}
 }
